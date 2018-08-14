267ce919cdc5f679a5f8a8e4be7ce0daf984981e
daidorian09 <yigitat1992@gmail.com>
2018-08-14 20:17:18 +0300

    Remove pre-defined redux-chrome-extension variable, load redux-devtools-extension

--- src/redux/redux.store.js
+++ src/redux/redux.store.js
@@ -2,24 +2,18 @@ import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from '../reducers/root.reducer'
 import { loadState, saveState } from './localStorage'
+import { composeWithDevTools } from 'redux-devtools-extension'
 const persistedState = loadState()
-
-const initialState = {}
 
 const middleware = [thunk]
 
-const store = createStore(
-    rootReducer, 
-    persistedState, 
-    compose(
-        applyMiddleware(...middleware), 
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-)
+const store = createStore(rootReducer, persistedState, composeWithDevTools(
+    applyMiddleware(...middleware)
+))
 
 store.subscribe(() => {
     // saves states to localStorage everytime state changes
     saveState(store.getState());
-  });
+  })
 
 export default store
